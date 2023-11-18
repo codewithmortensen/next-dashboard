058bd690caa5f15adcd00452c8c3fdc56faf25b5
Mortensen Ulysse <codewithmortensen@gmail.com>
2023-11-18 07:54:35 -0500

    Project Set up

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Source_Code_Pro, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const code = Source_Code_Pro({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
